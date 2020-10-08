bc059c4481cf3306c17721917a82ce00024c7aad
UnderscoreAnti <UnderscoreAnti@gmail.com>
2020-10-08 02:03:00 -0600

    Entry Node has new functions, naming the entry has now been added, setting the date of created and edited has been added.

--- Code/Entry.cs
+++ Code/Entry.cs
@@ -5,14 +5,36 @@ using System.Collections.Generic;
 public class Entry : Node2D
 {
     [Export]
-    string EntryName;
-    string DateEntryCreated;
-    string DateEntryEdited;
+    string EntryName, DateEntryCreated, DateEntryEdited;
     int EntryListCount;
     List<string> ListEntries = new List<string>();
 
     public override void _Ready()
     {
-        DateEntryCreated = Convert.ToString(DateTime.Now);
+        string getout = SetDate();
+        GD.Print(getout);
+        getout = SetDate(true);
+        GD.Print(getout);
+    }
+
+    public void AppendEntry(List<string> LE)
+    {
+        //
+    }
+
+    public string SetDate(bool WithTime = false)
+    {
+        string DateSet;
+        DateTime thisDay = DateTime.Today;
+        DateSet = thisDay.ToString("d");
+        if(WithTime)
+            DateSet = thisDay.ToString("g");
+
+        return DateSet;
+    }
+
+    public void DeleteEntry(List<string> LE)
+    {
+        //
     }
 }
